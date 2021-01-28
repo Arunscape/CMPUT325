2604578fec3e21709597def8e9bc66b65ba20f35
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-01-28 16:09:25 -0700

    gen subsets works now

--- assignments/assignment1/assignment1.lisp
+++ assignments/assignment1/assignment1.lisp
@@ -3,10 +3,10 @@
 ;this also tests for lists being members of lists
 ;both X and Y may be NIL, or lists containing NIL
 ;
-;here's how it works: 
+;here's how it works:
 ;returns nil if Y is nil
 ;returns true if x = f(y)
-;else return xmember(x, r(y))  
+;else return xmember(x, r(y))
 (defun xmember (X Y)
   (cond
     ((not Y) nil)
@@ -20,7 +20,7 @@
     ((not X) nil)
     ((atom (car X)) (cons (car X) (flatten (cdr X))))
     (t (append (flatten (car X)) (flatten (cdr X))))))
-  
+
 ;QUESTION 3
 (defun remove-duplicate (X)
   (cond
@@ -32,17 +32,27 @@
 ; it seems to work without checking if l1 is null todo, trace execution and find out why
 (defun mix (L1 L2)
   (cond
-    ((not L2) L1)
-;    ((not L1) L2)
-    (t (cons (car L1) (mix L2 (cdr L1))))))
-    
-    
+   ((not L2) L1)
+;  ((not L1) L2)
+   (t (cons (car L1) (mix L2 (cdr L1))))))
+
 ;QUESTION 5
 (defun allsubsets (L)
-  (gen-subsets (list nil) L))
+  (if (not L) (list nil)
+    (gen-subsets (car L) (allsubsets (cdr L)))))
 
-(defun gen-subsets (acc L)
-  (if (not L) acc
-      (append (gen-subsets  ((list (car L)) cdr L)))))
-  
-; for question 6 you probably want an accumulator
+; wikipedia notation https://en.wikipedia.org/wiki/Power_set#Recursive_definition
+; union of a power set of T and a power set of T whose each element is expanded with e
+; For a non-empty set S (assignment calls it L)
+; let e be any element of the set (car L)
+; and T is the relative complement of e;
+; then the power set of S is a union of a power set of T and
+; a power set of T whose each element is expanded with e
+(defun gen-subsets (e Tcomplement)
+  (if
+   (not tcomplement) nil
+   (cons
+     (cons e (car Tcomplement))
+     (cons (car Tcomplement) (gen-subsets e (cdr Tcomplement))))))
+
+
--- assignments/assignment1/test.lisp
+++ assignments/assignment1/test.lisp
@@ -28,3 +28,8 @@
 (test-case 4.4 (mix '(1 2 3) nil) '(1 2 3))
 (test-case 4.5 (mix '(1 2 3) '(nil)) '(1 nil 2 3))
 (test-case 4.6 (mix '(1 3 5) '(2 4 6 7 8 9)) '(1 2 3 4 5 6 7 8 9))
+
+(test-case 5.1 (allsubsets nil) '(nil))
+(test-case 5.2 (allsubsets '(a)) '((a) nil))
+(test-case 5.3 (allsubsets '(a b)) '((a b) (b) (a)  nil))
+(test-case 5.4 (allsubsets '(a b c)) '((a b c) (b c) (a c) (c) (a b) (b) (a) () ))
